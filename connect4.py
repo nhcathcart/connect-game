290aad636005c2e23380ee25504f261251d4712b
Nicholas Cathcart <nicholascathcart@nicholass-air.lan>
2022-02-01 13:19:57 -0500

    Globals now class, added  dimensions function, check for full

--- connect4.py
+++ connect4.py
@@ -1,51 +1,52 @@
-
 from cmath import inf
 
-#global variables
 board =  {}
-piece1 = 'X'
-piece2 = 'O'
-num_rows = 0
-num_cols = 0
-connect_to_win = 0
-valid_columns = set()
+
 
 # board example {(0, 0): ' ', (0, 1): ' ', (0, 2): ' ', (0, 3): ' ', 
 #                (1, 0): ' ', (1, 1): ' ', (1, 2): ' ', (1, 3): ' ',
 #                (2, 0): ' ', (2, 1): ' ', (2, 2): ' ', (2, 3): ' ',
 #                (3, 0): ' ', (3, 1): ' ', (3, 2): ' ', (3, 3): ' ',  }
 
-def make_board():
-    global num_rows, num_cols, connect_to_win
-    num_rows = input('Choose number of rows between 1-10: ')
-    if num_rows not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-        print('Oops. Invalid input. Try again.')
-        return make_board()
-    num_cols = input('Choose number of columns between 1-10: ')
-    if num_cols not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-        print('Oops. Invalid input. Try again.')
-        return make_board()
-    connect_to_win = input('Choose number to connect for a win between 1-10: ')
-    if connect_to_win not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-        print('Oops. Invalid input. Try again.')
-        return make_board()
-    num_rows = int(num_rows)
-    num_cols = int(num_cols)
-    connect_to_win = int(connect_to_win)
-    for i in range(num_rows):
-        for j in range(num_cols):
-            board[i, j] = ' '
+class GameParameters:
+    def __init__(self, num_rows=0, num_cols=0, connect_to_win=0, piece1='X', piece2='O', valid_columns=set()):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.connect_to_win = connect_to_win
+        self.piece1 = piece1
+        self.piece2 = piece2
+        self.valid_columns = valid_columns
+    
+    def make_board(self):
+        
+        self.num_rows = get_dimension_input('Choose number of rows between 1-10: ')
+        self.num_cols = get_dimension_input('Choose number of columns between 1-10: ')
+        self.connect_to_win = get_dimension_input('Choose number of stones to connect for a win between 1-10: ')
+        self.num_rows = int(self.num_rows)
+        self.num_cols = int(self.num_cols)
+        self.connect_to_win = int(self.connect_to_win)
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                board[i, j] = ' '
 
-def valid_columns_maker():
-    global valid_columns
-    for i in range(num_cols):
-        valid_columns.add(str(i))
+    def valid_columns_maker(self):
+        for i in range(self.num_cols):
+            self.valid_columns.add(str(i))
+
+game = GameParameters()
+
+def get_dimension_input(text):
+  num = input(text)
+  if num in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
+    return int(num)
+  print('Oops. Invalid input. Try again.')
+  return get_dimension_input(text)
         
 def print_board(board):
+    num_cols = game.num_cols
+    num_rows = game.num_rows
     rows = []
-    row_break = ''
-    for _ in range(num_cols):
-        row_break += ' ---'
+    row_break = ' ---' * num_cols
     for i in range(num_rows):
         row = '| '
         for j in range(num_cols):
@@ -63,28 +64,43 @@ def print_board(board):
         print(row)
 
 def user1_play():
+    valid_columns = game.valid_columns
+    num_rows = game.num_rows
+    piece1 = game.piece1
     col = input('Choose your column: ')
     if col not in valid_columns:
         print('Oops thats not a valid column. Try again.')
         return user1_play()
     col = int(col)
+    if board[0, col] != ' ':
+        print('Oops. That column is full. Try again.')
+        return user1_play()
     for i in range(num_rows-1, -1, -1):
         if board[i, col] == ' ':
             board[i, col] = piece1
             break
 
 def user2_play():
+    valid_columns = game.valid_columns
+    piece2 = game.piece2
+    num_rows = game.num_rows
     col = input('Choose your column: ')
     if col not in valid_columns:
-        print('Oops thats not a vaid column. Try again.')
+        print('Oops thats not a valid column. Try again.')
         return user2_play()
     col = int(col)
+    if board[0, col] != ' ':
+        print('Oops. That column is full. Try again.')
+        return user2_play()
     for i in range(num_rows-1, -1, -1):
         if board[i, col] == ' ':
             board[i, col] = piece2
             break
 
 def ai_play():
+    num_rows = game.num_rows
+    num_cols = game.num_cols
+    piece2 = game.piece2
     best_score = -inf
     best_play = None
     for j in range(num_cols):
@@ -103,6 +119,10 @@ def ai_play():
     return
 
 def minimax(board, depth, isMaximizing, alpha, beta):
+    num_rows = game.num_rows
+    num_cols = game.num_cols
+    piece1 = game.piece1
+    piece2 = game.piece2
     if check_player_2_win() == True:
         return 1000
     if check_player_1_win() == True:
@@ -152,6 +172,11 @@ def minimax(board, depth, isMaximizing, alpha, beta):
         return best_score
 
 def eval_board(board):
+    num_rows = game.num_rows
+    num_cols = game.num_cols
+    connect_to_win = game.connect_to_win
+    piece1 = game.piece1
+    piece2 = game.piece2
     board_score = 0
     # check verticals
     for j in range(num_cols):
@@ -222,6 +247,10 @@ def eval_board(board):
     return board_score
 
 def check_player_1_win():
+    num_rows = game.num_rows
+    num_cols = game.num_cols
+    connect_to_win = game.connect_to_win
+    piece1 = game.piece1
     # check verticals
     for j in range(num_cols):
         for i in range(num_rows - connect_to_win + 1):
@@ -258,6 +287,10 @@ def check_player_1_win():
     return False
 
 def check_player_2_win():
+    num_rows = game.num_rows
+    num_cols = game.num_cols
+    connect_to_win = game.connect_to_win
+    piece2 = game.piece2
     # check verticals
     for j in range(num_cols):
         for i in range(num_rows - connect_to_win + 1):
@@ -300,8 +333,8 @@ def check_draw():
     return True
 
 def gameplay():
-    make_board()
-    valid_columns_maker()
+    game.make_board()
+    game.valid_columns_maker()
     while check_draw() == False:
         
         print_board(board)
@@ -330,8 +363,8 @@ def gameplay():
     print('Its a draw!')
     
 def computer_gameplay():
-    make_board()
-    valid_columns_maker()
+    game.make_board()
+    game.valid_columns_maker()
     while check_draw() == False:
         
         print_board(board)
